271eac0df2efc830eb3bcb2258aa29561d158a2f
dachuiwang <dachuiwang@dachuiwangdeiMac.local>
2018-04-21 19:12:29 +0800

    cc

--- WLBook.h
+++ WLBook.h
@@ -24,20 +24,8 @@
 @property(nonatomic, strong)NSString *updateDate;
 @property(nonatomic, strong)NSString *site;
 
-// 搜狗特有的字段
-@property(nonatomic, strong)NSString *sougou_bkey;
-@property(nonatomic, strong)NSString *sougou_id;
-@property(nonatomic, strong)NSString *sougou_md;
-@property(nonatomic, strong)NSString *sougou_authorMD5;
-@property(nonatomic, strong)NSString *sougou_nameMD5;
-@property(nonatomic, strong)NSString *sougou_status; // 0 连载 1 完结
+// 追书特有字段
+@property(nonatomic, strong)NSString *zhuishu_id;
 
-//阅读章节index
-@property(nonatomic, strong)NSString *readChapterIndex;
-//阅读页数index
-@property(nonatomic, strong)NSString *readOffset;
-
-//缓存大小
-@property(nonatomic, strong)NSString *cacheSize;
 
 @end
